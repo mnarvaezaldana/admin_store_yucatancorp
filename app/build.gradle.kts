924f5f0f64fd36283321f89578b35ab9036db47b
MarcosArmando <narvaezaldana@gmail.com>
2025-02-22 23:30:09 -0600

    ADMINSTORE-002 remove unneeded change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,4 +91,5 @@ dependencies {
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
-    debugI
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
