df80ce977519699102f7554187e0b32ae1eb0cbe
mnarvaezaldana <narvaezaldana@gmail.com>
2025-02-23 19:07:44 -0600

    ADMINSTORE-003-01 (#7)
    * ADMINSTORE-003-01 Create string values for hints and fields
    
    * ADMINSTORE-003-01 create composables for login screen for fields and logo

--- app/src/main/java/com/yucatancorp/adminstore/MainActivity.kt
+++ app/src/main/java/com/yucatancorp/adminstore/MainActivity.kt
@@ -3,13 +3,31 @@ package com.yucatancorp.adminstore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.yucatancorp.adminstore.ui.theme.AdminStoreTheme
@@ -17,20 +35,59 @@ import com.yucatancorp.adminstore.ui.theme.AdminStoreTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         installSplashScreen()
         setContent {
             AdminStoreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LoginScreen()
             }
         }
     }
 }
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color.Green),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "login_image_icon",
+            contentScale = ContentScale.Crop,
+        )
+        LoginTextField(R.string.user_email, false, false)
+        LoginTextField(R.string.user_password, true, true)
+    }
+}
+
+@Composable
+fun LoginTextField(stringValue: Int, hasToggle: Boolean, hasVisualTransformation: Boolean) {
+    var text by remember { mutableStateOf("") }
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text(stringResource(id = stringValue)) },
+        placeholder = { Text(stringResource(id = stringValue)) },
+        singleLine = true,
+        visualTransformation = if (hasVisualTransformation) {
+            if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
+        } else { VisualTransformation.None },
+        trailingIcon = {
+            if (hasToggle) {
+                IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                    Icon(
+                        imageVector = if (isPasswordVisible) Icons.Default.Add else Icons.Default.Call,
+                        contentDescription = if (isPasswordVisible) "Hide password" else "Show password"
+                    )
+                }
+            }
+        }
+    )
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -38,6 +95,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
 }
 
 @Preview(showBackground = true)
